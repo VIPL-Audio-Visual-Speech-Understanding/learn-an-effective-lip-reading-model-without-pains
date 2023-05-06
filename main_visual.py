26ab8011d268c1a15b0f2f5e30ee18bcd9cacdb2
aviad_dv <aviad.shimoni@doubleverify.com>
2023-05-06 16:54:54 +0300

    remove optional arguments for now, no need

--- main_visual.py
+++ main_visual.py
@@ -39,10 +39,10 @@ parser.add_argument('--save_prefix', type=str, required=True)
 
 # dataset
 parser.add_argument('--dataset', type=str, required=True)
-parser.add_argument('--border', type=str2bool, required=True)
-parser.add_argument('--mixup', type=str2bool, required=True)
-parser.add_argument('--label_smooth', type=str2bool, required=True)
-parser.add_argument('--se', type=str2bool, required=True)
+# parser.add_argument('--border', type=str2bool, required=True)
+# parser.add_argument('--mixup', type=str2bool, required=True)
+# parser.add_argument('--label_smooth', type=str2bool, required=True)
+# parser.add_argument('--se', type=str2bool, required=True)
 
 args = parser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
@@ -79,7 +79,7 @@ lr = args.batch_size / 32.0 / torch.cuda.device_count() * args.lr
 optim_video = optim.Adam(video_model.parameters(), lr=lr, weight_decay=1e-4)
 scheduler = optim.lr_scheduler.CosineAnnealingLR(optim_video, T_max=args.max_epoch, eta_min=5e-6)
 
-if (args.weights is not None):
+if args.weights is not None:
     print('load weights')
     weight = torch.load(args.weights, map_location=torch.device('cpu'))
     load_missing(video_model, weight.get('video_model'))
@@ -112,12 +112,7 @@ def test():
 
         print('start testing')
         v_acc = []
-        entropy = []
-        acc_mean = []
         total = 0
-        cons_acc = 0.0
-        cons_total = 0.0
-        attns = []
 
         for (i_iter, input) in enumerate(loader):
 
@@ -127,18 +122,13 @@ def test():
             video = input.get('video').cuda(non_blocking=True)
             label = input.get('label').cuda(non_blocking=True)
             total = total + video.size(0)
-            names = input.get('name')
-            border = input.get('duration').cuda(non_blocking=True).float()
 
             with autocast():
-                if (args.border):
-                    y_v = video_model(video, border)
-                else:
-                    y_v = video_model(video)
+                y_v = video_model(video)
 
             v_acc.extend((y_v.argmax(-1) == label).cpu().numpy().tolist())
             toc = time.time()
-            if (i_iter % 10 == 0):
+            if i_iter % 10 == 0:
                 msg = ''
                 msg = add_msg(msg, 'v_acc={:.5f}', np.array(v_acc).reshape(-1).mean())
                 msg = add_msg(msg, 'eta={:.5f}', (toc - tic) * (len(loader) - i_iter) / 3600.0)
@@ -165,21 +155,10 @@ def train():
     loader = dataset2dataloader(dataset, args.batch_size, args.num_workers)
 
     max_epoch = args.max_epoch
-
-    ce = nn.CrossEntropyLoss()
-
     tot_iter = 0
     best_acc = 0.0
-    adjust_lr_count = 0
-    alpha = 0.2
-    beta_distribution = torch.distributions.beta.Beta(alpha, alpha)
     scaler = GradScaler()
     for epoch in range(max_epoch):
-        total = 0.0
-        v_acc = 0.0
-        total = 0.0
-
-        lsr = LSR()
 
         for (i_iter, input) in enumerate(loader):
             tic = time.time()
@@ -187,37 +166,13 @@ def train():
             video_model.train()
             video = input.get('video').cuda(non_blocking=True)
             label = input.get('label').cuda(non_blocking=True).long()
-            border = input.get('duration').cuda(non_blocking=True).float()
 
             loss = {}
 
-            if (args.label_smooth):
-                loss_fn = lsr
-            else:
-                loss_fn = nn.CrossEntropyLoss()
+            loss_fn = nn.CrossEntropyLoss()
 
             with autocast():
-                if (args.mixup):
-                    lambda_ = np.random.beta(alpha, alpha)
-                    index = torch.randperm(video.size(0)).cuda(non_blocking=True)
-
-                    mix_video = lambda_ * video + (1 - lambda_) * video[index, :]
-                    mix_border = lambda_ * border + (1 - lambda_) * border[index, :]
-
-                    label_a, label_b = label, label[index]
-
-                    if (args.border):
-                        y_v = video_model(mix_video, mix_border)
-                    else:
-                        y_v = video_model(mix_video)
-
-                    loss_bp = lambda_ * loss_fn(y_v, label_a) + (1 - lambda_) * loss_fn(y_v, label_b)
-
-                else:
-                    if (args.border):
-                        y_v = video_model(video, border)
-                    else:
-                        y_v = video_model(video)
+                    y_v = video_model(video)
 
                     loss_bp = loss_fn(y_v, label)
 
@@ -238,22 +193,22 @@ def train():
             msg = msg + str(',best_acc={:2f}'.format(best_acc))
             print(msg)
 
-            if (i_iter == len(loader) - 1 or (epoch == 0 and i_iter == 0)):
+            if i_iter == len(loader) - 1 or (epoch == 0 and i_iter == 0):
 
                 acc, msg = test()
 
-                if (acc > best_acc):
-                    savename = '{}_iter_{}_epoch_{}_{}.pt'.format(args.save_prefix, tot_iter, epoch, msg)
+                if acc > best_acc:
+                    saved_file = '{}_iter_{}_epoch_{}_{}.pt'.format(args.save_prefix, tot_iter, epoch, msg)
 
-                    temp = os.path.split(savename)[0]
+                    temp = os.path.split(saved_file)[0]
                     if (not os.path.exists(temp)):
                         os.makedirs(temp)
                     torch.save(
                         {
                             'video_model': video_model.module.state_dict(),
-                        }, savename)
+                        }, saved_file)
 
-                if (tot_iter != 0):
+                if tot_iter != 0:
                     best_acc = max(acc, best_acc)
 
             tot_iter += 1

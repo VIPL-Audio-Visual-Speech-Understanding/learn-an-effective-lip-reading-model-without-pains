66bb0f9a4439fc6f8cb763b18166b11236a56a40
aviad_dv <aviad.shimoni@doubleverify.com>
2023-05-06 14:33:41 +0300

    video cnn push

--- model/video_cnn.py
+++ model/video_cnn.py
@@ -1,21 +1,5 @@
-# coding: utf-8
 import math
-import numpy as np
-
-
-import torch
 import torch.nn as nn
-from torch.autograd import Variable
-import torch.nn.functional as F
-
-
-def conv3x3(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
-
-
-def conv1x1(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1)
 
 
 class BasicBlock(nn.Module):
@@ -23,70 +7,77 @@ class BasicBlock(nn.Module):
 
     def __init__(self, inplanes, planes, stride=1, downsample=None, se=False):
         super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
+
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
         self.se = se
-        
-        if(self.se):
-            self.gap = nn.AdaptiveAvgPool2d(1)
-            self.conv3 = conv1x1(planes, planes//16)
-            self.conv4 = conv1x1(planes//16, planes)
+
+        if self.se:
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+            self.fc1 = nn.Conv2d(planes, planes // 16, kernel_size=1, bias=False)
+            self.fc2 = nn.Conv2d(planes // 16, planes, kernel_size=1, bias=False)
+            self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        residual = x
+        identity = x
+
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
+
         out = self.conv2(out)
         out = self.bn2(out)
-        
+
         if self.downsample is not None:
-            residual = self.downsample(x)
-            
-        if(self.se):
-            w = self.gap(out)
-            w = self.conv3(w)
+            identity = self.downsample(x)
+
+        if self.se:
+            w = self.avgpool(out)
+            w = self.fc1(w)
             w = self.relu(w)
-            w = self.conv4(w).sigmoid()
-            
+            w = self.fc2(w)
+            w = self.sigmoid(w)
             out = out * w
-        
-        out = out + residual
+
+        out += identity
         out = self.relu(out)
 
         return out
 
 
 class ResNet(nn.Module):
-
     def __init__(self, block, layers, se=False):
+        super().__init__()
         self.inplanes = 64
-        super(ResNet, self).__init__()
         self.se = se
+
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        
-        
+
         self.avgpool = nn.AdaptiveAvgPool2d(1)
-        
-        self.bn = nn.BatchNorm1d(512)
+        self.fc = nn.Linear(512 * block.expansion, 10)
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm1d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -106,35 +97,44 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
+
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        x = self.bn(x)
-        return x        
+        x = self.fc(x)
+
+        return x
 
 
 class VideoCNN(nn.Module):
     def __init__(self, se=False):
         super(VideoCNN, self).__init__()
-        
+
         # frontend3D
         self.frontend3D = nn.Sequential(
-                nn.Conv3d(1, 64, kernel_size=(5, 7, 7), stride=(1, 2, 2), padding=(2, 3, 3), bias=False),
-                nn.BatchNorm3d(64),
-                nn.ReLU(True),
-                nn.MaxPool3d(kernel_size=(1, 3, 3), stride=(1, 2, 2), padding=(0, 1, 1))
-                )
+            nn.Conv3d(1, 64, kernel_size=(5, 7, 7), stride=(1, 2, 2), padding=(2, 3, 3), bias=False),
+            nn.BatchNorm3d(64),
+            nn.ReLU(True),
+            nn.MaxPool3d(kernel_size=(1, 3, 3), stride=(1, 2, 2), padding=(0, 1, 1))
+        )
+
         # resnet
         self.resnet18 = ResNet(BasicBlock, [2, 2, 2, 2], se=se)
+
+        # dropout
         self.dropout = nn.Dropout(p=0.5)
 
-        # backend_gru
         # initialize
         self._initialize_weights()
-    
+
     def visual_frontend_forward(self, x):
         x = x.transpose(1, 2)
         x = self.frontend3D(x)
@@ -142,47 +142,27 @@ class VideoCNN(nn.Module):
         x = x.contiguous()
         x = x.view(-1, 64, x.size(3), x.size(4))
         x = self.resnet18(x)
-        return x        
-    
+        return x
+
     def forward(self, x):
         b, t = x.size()[:2]
 
         x = self.visual_frontend_forward(x)
-        
-        #x = self.dropout(x)
-        feat = x.view(b, -1, 512)
 
-        x = x.view(b, -1, 512)       
+        x = self.dropout(x)
+        x = x.view(b, -1, 512)
+
         return x
 
     def _initialize_weights(self):
         for m in self.modules():
-            if isinstance(m, nn.Conv3d):
+            if isinstance(m, nn.Conv3d) or isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv1d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.kernel_size[2] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
                 if m.bias is not None:
                     m.bias.data.zero_()
 
-            elif isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-
-            elif isinstance(m, nn.Conv1d):
-                n = m.kernel_size[0] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-
-            elif isinstance(m, nn.BatchNorm3d):
+            elif isinstance(m, nn.BatchNorm3d) or isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-            elif isinstance(m, nn.BatchNorm1d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()

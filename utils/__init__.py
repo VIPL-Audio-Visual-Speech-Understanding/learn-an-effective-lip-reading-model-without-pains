6a9404ae5f61ff3590fa598c0cf1ba03f7187d08
aviad_dv <aviad.shimoni@doubleverify.com>
2023-05-07 21:42:49 +0300

    optimize utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,2 @@
 from .dataset import LRWDataset
-from .dataset_lrw1000 import LRW1000_Dataset
 from .cvtransforms import *
--- utils/cvtransforms.py
+++ utils/cvtransforms.py
@@ -4,21 +4,8 @@ import cv2
 import numpy as np
 import torch
 
-def TensorRandomFlip(tensor):
-    # (b, c, t, h, w)
-    if(random.random() > 0.5):
-        return torch.flip(tensor, dims=[4])        
-    return tensor        
 
-def TensorRandomCrop(tensor, size):
-    h, w = tensor.size(-2), tensor.size(-1)
-    tw, th = size
-    x1 = random.randint(0, w - tw)
-    y1 = random.randint(0, h - th)
-    return tensor[:,:,:,x1:x1+th, y1:y1+w]
-
-
-def CenterCrop(batch_img, size):
+def center_crop(batch_img, size):
     w, h = batch_img.shape[2], batch_img.shape[1]
     th, tw = size
     img = np.zeros((batch_img.shape[0], th, tw))
@@ -27,7 +14,8 @@ def CenterCrop(batch_img, size):
     img = batch_img[:, y1:y1+th, x1:x1+tw]
     return img
 
-def RandomCrop(batch_img, size):
+
+def random_crop(batch_img, size):
     w, h = batch_img.shape[2], batch_img.shape[1]
     th, tw = size
     img = np.zeros((batch_img.shape[0], th, tw))
@@ -36,7 +24,8 @@ def RandomCrop(batch_img, size):
     img = batch_img[:,y1:y1+th,x1:x1+tw]
     return img
 
-def HorizontalFlip(batch_img):
+
+def horizontal_flip(batch_img):
     if random.random() > 0.5:
         batch_img = np.ascontiguousarray(batch_img[:,:,::-1])
     return batch_img
